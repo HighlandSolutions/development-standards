d3435a9cb9e83966878779008b4560ef9e7abc7f
Curtis Blackwell <blackwell.curtis@gmail.com>
2020-05-11 13:28:44 -0500

    tabs :arrow_right: spaces

--- standards/assets/.eslintrc.js
+++ standards/assets/.eslintrc.js
@@ -2,9 +2,9 @@ module.exports = {
   root: true,
 
   extends: [
-		// Uncomment the next line if this project uses Vue.
-		// 'plugin:vue/essential',
-		// This should come last unless there's a specific reason to override our config with another.
+    // Uncomment the next line if this project uses Vue.
+    // 'plugin:vue/essential',
+    // This should come last unless there's a specific reason to override our config with another.
     'highland',
   ],
 
@@ -12,16 +12,16 @@ module.exports = {
     browser: true,
   },
 
-	// Add any global functions/values here.
+  // Add any global functions/values here.
   globals: {
-		// E.g. route() is a helper that enables referencing Laravel-defined routes in javascript.
-		// https://journal.highlandsolutions.com/how-i-like-to-simplify-ziggys-route-helper-59127e19d4ba
+    // E.g. route() is a helper that enables referencing Laravel-defined routes in javascript.
+    // https://journal.highlandsolutions.com/how-i-like-to-simplify-ziggys-route-helper-59127e19d4ba
     // route: true,
   },
 
   settings: {
-		// If you have issues with the import plugin's reolution, try uncommenting and tweaking this.
-		// https://github.com/benmosher/eslint-plugin-import
+    // If you have issues with the import plugin's reolution, try uncommenting and tweaking this.
+    // https://github.com/benmosher/eslint-plugin-import
     // 'import/resolver': {
     //   node: {
     //     extensions: [
